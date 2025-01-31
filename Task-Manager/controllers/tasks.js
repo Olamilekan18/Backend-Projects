8fd0122a807bacbd9d22282f65359acd94250503
Olamilekan Kareem <olamilekankareem717@gmail.com>
2025-02-01 00:07:54 +0100

    learnt mongoose

--- Task-Manager/controllers/tasks.js
+++ Task-Manager/controllers/tasks.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = express.Router()
-
+const Task = require('../models/task.js')
 const getAllTasks = (req, res) => {
     res.send('All items from the file')
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+    // const task = await Task.create({name: req.body.name, completed: req.body.completed})
+    res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+        // res.status(error.status).json({msg: error.message})
+    }
+    
 }
 
 const getTask = (req, res) => {
